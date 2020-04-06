72c26382e5d0cb42d197607ab94186a64d5fb1b1
fedor-g <57702675+fedor-g@users.noreply.github.com>
2020-04-06 21:38:22 +0300

    Br 1 (#1)
    PR1_squashed

---
+++ src/main/java/com/test/first/You.java
@@ -0,0 +1,13 @@
+package com.test.first;
+
+public class You {
+
+  public static void main(String[] args) {
+    System.out.println("LOL");
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    System.out.println();
+  }
+
+}
